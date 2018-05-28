d64d097dfeb4308a73243974b957794424f6cf31
conor <conor.sheehan@live.ie>
2018-05-28 19:39:21 +0100

    update travis.yml to install gems, add check for linkedin title

--- spec/home_page_spec.rb
+++ spec/home_page_spec.rb
@@ -2,13 +2,15 @@ require "faraday"
 
 describe "home page", type: :feature, js: true do
 
-  it "has a title" do
+  before(:each) do
     visit '/'
+  end
+
+  it "has a title" do
     expect(page).to have_title "Conor Sheehan"
   end
 
   it "does not throw 400/500 error for any link" do
-    visit '/'
     within(".wrapper") do
       exclude_links = ['more','View Source on GitHub']
       links = all('a').select {|l| !exclude_links.any? { |word| l.text.include?(word) }}
@@ -21,6 +23,20 @@ describe "home page", type: :feature, js: true do
         # only check for typical client and server side errors
         expect((400..500)).not_to include(http_status) 
       end
+    end
+  end
+
+  context "header" do
+    # should catch case where my profile link changes
+    it "has a valid linked-in link" do
+      linked_in_link = page.find(:xpath, 
+        "//div[contains(@class, 'col')]//span[text()='Linked In']"
+        )
+      linked_in_link.click()
+      sleep(2)
+      title = page.title.downcase()
+      expect(title).to include("conor")
+      expect(title).to include("linkedin")
     end
   end
 
